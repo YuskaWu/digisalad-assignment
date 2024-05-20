2aa9db6e74affe9fbc2e348f87425886d510e6ef
Yuska <yuska@msn.com>
2024-05-20 09:50:32 +0800

    feature: implement side menu

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import SideMenu from './SideMenu.vue'
 import { onMounted, onUnmounted, ref } from '#imports'
-import CloseSvg from '@/assets/icon/close.svg'
 import MenuSvg from '@/assets/icon/menu.svg'
 import LogoSvg from '@/assets/image/logo.svg'
 import { cn } from '@/utils/tailwind'
 
+const isMenuVisible = ref(false)
 const hasScrolled = ref(false)
 
 function onScroll() {
@@ -13,6 +13,7 @@ function onScroll() {
 }
 
 onMounted(() => {
+  hasScrolled.value = !!document.documentElement.scrollTop
   window.addEventListener('scroll', onScroll)
 })
 
@@ -23,11 +24,11 @@ onUnmounted(() => {
 
 <template>
   <header
-    :class="cn('fixed left-0 top-0 z-10 h-20 w-full bg-transparent transition-colors', hasScrolled && 'bg-white shadow-md')"
+    :class="cn('duration-500 fixed left-0 top-0 z-10 h-20 w-full bg-transparent transition-colors', (hasScrolled && !isMenuVisible) && 'bg-white shadow-md')"
   >
-    <nav class="m-auto flex size-full max-w-[90rem] items-center px-8 lg:px-20">
+    <nav :class="cn('m-auto flex size-full max-w-[90rem] items-center px-8 transition-colors lg:px-20', isMenuVisible && 'opacity-0')">
       <LogoSvg
-        :class="cn('max-w-[7.5rem] text-primary opacity-0 transition-opacity', hasScrolled && 'opacity-100')"
+        :class="cn('duration-300 max-w-[7.5rem] text-primary opacity-0 transition-opacity', hasScrolled && 'opacity-100')"
       />
 
       <button class="ml-auto rounded-full bg-gradient-to-r from-[#4ee5ea] to-[#26d0a8] px-4 py-3 text-[0.875rem]/[1.0625rem] tracking-[0.08em] text-white">
@@ -35,11 +36,12 @@ onUnmounted(() => {
       </button>
 
       <button class="ml-8 lg:ml-12">
-        <CloseSvg class="hidden w-[1.875rem] text-white" />
-        <MenuSvg :class="cn('w-[1.875rem] text-white', hasScrolled && 'text-secondary')" />
+        <MenuSvg
+          :class="cn('duration-300 w-[1.875rem] transition-colors text-white', hasScrolled && 'text-secondary')"
+          @click="isMenuVisible = !isMenuVisible"
+        />
       </button>
     </nav>
-
-    <SideMenu />
   </header>
+  <SideMenu v-model:visible="isMenuVisible" />
 </template>
--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -1,5 +1,159 @@
+<script setup lang="ts">
+import CloseSvg from '@/assets/icon/close.svg'
+import LogoSvg from '@/assets/image/logo.svg'
+import BgImage1Url from '@/assets/image/menu-item-bg-1.webp?url'
+import BgImage2Url from '@/assets/image/menu-item-bg-2.webp?url'
+import BgImage3Url from '@/assets/image/menu-item-bg-3.webp?url'
+import BgImage4Url from '@/assets/image/menu-item-bg-4.webp?url'
+import BgImage5Url from '@/assets/image/menu-item-bg-5.webp?url'
+
+import CheeseSvgUrl from '@/assets/image/ingredient-cheese.svg?url'
+import GherkinSvgUrl from '@/assets/image/ingredient-gherkin.svg?url'
+import LettuceSvgUrl from '@/assets/image/ingredient-lettuce.svg?url'
+import TomatoSvgUrl from '@/assets/image/ingredient-tomato.svg?url'
+import WatermelonSvgUrl from '@/assets/image/ingredient-watermelon.svg?url'
+
+import { ref, watch } from '#imports'
+
+defineEmits<{ 'update:visible': [visible: boolean] }>()
+const visible = defineModel<boolean>('visible')
+
+const root = ref<HTMLDivElement>()
+
+watch(visible, () => {
+  if (!root.value) {
+    return
+  }
+
+  if (visible.value) {
+    root.value.style.zIndex = '20'
+    root.value.style.opacity = '100'
+  }
+  else {
+    root.value.style.opacity = '0'
+    setTimeout(() => {
+      root.value!.style.zIndex = '-1'
+    }, 300)
+  }
+})
+</script>
+
 <template>
-  <div class="fixed left-0 top-0 z-20 hidden size-full bg-[rgba(255,255,255,0.5)]">
-    menu
+  <div
+    ref="root"
+    class="menu-bg fixed left-0 top-0 z-[-1] flex size-full items-center justify-center opacity-0 transition-opacity duration-300"
+  >
+    <div class="absolute left-0 top-0 w-full">
+      <div class="m-auto flex h-20 max-w-[90rem] items-center px-8 lg:px-20">
+        <LogoSvg class="max-w-[7.5rem] text-white" />
+        <button
+          class="ml-auto w-[1.875rem] text-white"
+          @click="$emit('update:visible', false)"
+        >
+          <CloseSvg class="w-[1.875rem]" />
+        </button>
+      </div>
+    </div>
+
+    <!-- menu -->
+    <div class="grid h-[80dvh] w-[80dvw] max-w-5xl grid-cols-1 items-center gap-6 text-white lg:grid-cols-[36%_28%_36%]">
+      <!-- column 1 -->
+      <div class="flex h-full justify-center gap-6 lg:flex-col ">
+        <div
+          class="flex w-full items-center rounded-3xl bg-cover bg-center p-8 lg:h-1/3"
+          :style="`background-image: url('${BgImage1Url}')`"
+        >
+          <div class="flex flex-wrap gap-6">
+            <img :src="LettuceSvgUrl">
+            <div class=" flex flex-col gap-2">
+              <span class="leading-5 tracking-[0.1125em]">EMPOWERING BRANDS</span>
+              <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+                ABOUT US
+              </span>
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex w-full items-end rounded-3xl bg-cover bg-center p-8 lg:h-[45%] "
+          :style="`background-image: url('${BgImage4Url}')`"
+        >
+          <div class="flex flex-wrap gap-6">
+            <img :src="TomatoSvgUrl">
+            <div class=" flex flex-col gap-2">
+              <span class="leading-5 tracking-[0.1125em]">CASE STUDIES</span>
+              <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+                WORKS
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- column 2 -->
+      <div class="flex h-full justify-center gap-6 lg:flex-col">
+        <div
+          class="flex w-full items-end rounded-3xl bg-cover bg-center p-8 lg:h-3/5"
+          :style="`background-image: url('${BgImage2Url}')`"
+        >
+          <div class="flex flex-wrap gap-6">
+            <img :src="CheeseSvgUrl">
+            <div class=" flex flex-col gap-2">
+              <span class="leading-5 tracking-[0.1125em]">BE COOL WITH US</span>
+              <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+                CAREERS
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="flex w-full items-end rounded-3xl bg-cover bg-center p-8 lg:h-2/5"
+          :style="`background-image: url('${BgImage5Url}')`"
+        >
+          <div class="flex flex-wrap gap-6">
+            <img :src="GherkinSvgUrl">
+            <div class=" flex flex-col gap-2">
+              <span class="leading-5 tracking-[0.1125em]">OUR STRATEGIES</span>
+              <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+                INSIGHTS
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- column 3 -->
+      <div class="flex h-full justify-center gap-6 lg:flex-col">
+        <div
+          class="flex w-full items-end rounded-3xl bg-cover bg-center p-8 lg:h-[45%]"
+          :style="`background-image: url('${BgImage3Url}')`"
+        >
+          <div class="flex flex-wrap gap-6">
+            <img :src="WatermelonSvgUrl">
+            <div class=" flex flex-col gap-2">
+              <span class="leading-5 tracking-[0.1125em]">AREAS OF EXPERTISE</span>
+              <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+                SERVICES
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="flex w-full flex-col justify-center gap-2 rounded-3xl bg-white  p-8 text-primary lg:h-1/3"
+        >
+          <span class="leading-5 tracking-[0.1125em] text-black">START YOUR JOURNEY WITH US</span>
+          <span class=" text-[2rem]/[2.5rem] font-bold tracking-[0.115em]">
+            CONTACT
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.menu-bg {
+  background-image: linear-gradient(to top, rgb(38 198 208 / 90%), rgb(35 1 38 / 90%));
+}
+</style>
